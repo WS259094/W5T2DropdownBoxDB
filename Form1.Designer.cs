f49d1d0115f0ccbfe8c7ae36f930cbc3d469636a
WS259094 <WS259094@Weston.ac.uk>
2019-10-17 13:14:48 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace W5T2DropdownBoxDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboSelect = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboSelect
+            // 
+            this.cboSelect.FormattingEnabled = true;
+            this.cboSelect.Location = new System.Drawing.Point(31, 65);
+            this.cboSelect.Name = "cboSelect";
+            this.cboSelect.Size = new System.Drawing.Size(121, 21);
+            this.cboSelect.TabIndex = 0;
+            this.cboSelect.SelectedIndexChanged += new System.EventHandler(this.CboSelect_SelectedIndexChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(31, 246);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(372, 66);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(372, 116);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 3;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(372, 168);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.Size = new System.Drawing.Size(100, 20);
+            this.txtAverage.TabIndex = 4;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(295, 69);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(295, 116);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(35, 13);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "Score";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(295, 168);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(47, 13);
+            this.lblAverage.TabIndex = 7;
+            this.lblAverage.Text = "Average";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(207, 255);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(66, 24);
+            this.lblError.TabIndex = 8;
+            this.lblError.Text = "label1";
+            this.lblError.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cboSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboSelect;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace W5T2DropdownBoxDB
+{
+
+
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+
+            string conString = "server=server1.logicalview.co.uk;user=c444WS259094;database=c444WS259094Colab;password=Friday@63;CharSet=utf8;";
+
+            MySqlConnection sqlConn = new MySqlConnection(conString);
+
+            string sqlStatement = "SELECT * FROM `tbl_Test`";
+
+            MySqlDataAdapter SqlDataAdapter = new MySqlDataAdapter(sqlStatement, sqlConn);
+
+            DataTable dtUsers = new DataTable();
+
+            sqlConn.Open();
+
+            SqlDataAdapter.Fill(dtUsers);
+
+            sqlConn.Close();
+
+
+
+            string sID = cboSelect.SelectedItem.ToString();
+
+            string sName = txtName.Text;
+            string sScore = txtScore.Text;
+            string sAverage = txtAverage.Text;
+
+
+
+            string sNameUpdate = "UPDATE `tbl_Test` SET `Name`=[`"+sName+"`] WHERE `ID` = " + sID;
+            string sScoreUpdate = "UPDATE `tbl_Test` SET `Score`=[`"+sScore+"`] WHERE `ID` =" + sID;
+            string sAverageUpdate = "UPDATE `tbl_Test` SET `Average`=[`"+sAverage+"`] WHERE `ID` =" + sID;
+
+            bool bScoreErrorCheck = int.TryParse(sScore, out int iResult);
+            bool bAverageErrorCheck = int.TryParse(sAverage, out int iResult2);
+            bool NamecontainsInt = sName.Any(char.IsDigit);
+
+            
+
+            if (NamecontainsInt == false)
+            {
+                MySqlCommand sqlComm = new MySqlCommand(sNameUpdate, sqlConn);
+            }
+            else
+            {
+                lblError.Text = "Error, name must not use numbers\n";
+            }
+
+            if (bScoreErrorCheck == true)
+            {
+                
+                MySqlCommand sqlComm = new MySqlCommand(sScoreUpdate, sqlConn);
+
+            }
+            else
+            {
+                lblError.Text = "Error, Score must only use Numbers\n";
+            }
+
+            if (bAverageErrorCheck == true)
+            {
+                MySqlCommand sqlComm = new MySqlCommand(sAverageUpdate, sqlConn);
+            }
+            else
+            {
+                lblError.Text = "Error, Average must only use Numbers\n";
+            }
+
+        }
+
+        private void CboSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string conString = "server=server1.logicalview.co.uk;user=c444WS259094;database=c444WS259094Colab;password=Friday@63;CharSet=utf8;";
+
+            MySqlConnection sqlConn = new MySqlConnection(conString);
+
+            string sID =  cboSelect.SelectedItem.ToString();
+
+            sqlConn.Open();
+            string sName = "SELECT `Name` FROM `tbl_Test` WHERE `ID` = " + sID;
+            string sScore = "SELECT `Score` FROM `tbl_Test` WHERE `ID` = "+ sID;
+            string sAverage = "SELECT `Average` FROM `tbl_Test` WHERE `ID` = " + sID;
+
+
+                MySqlCommand sqlComm1 = new MySqlCommand(sName, sqlConn);
+                sqlComm1.ExecuteNonQuery();
+
+
+                MySqlCommand sqlComm2 = new MySqlCommand(sScore, sqlConn);
+                sqlComm2.ExecuteNonQuery();
+            
+                MySqlCommand sqlComm3 = new MySqlCommand(sAverage, sqlConn);
+                sqlComm3.ExecuteNonQuery();
+
+            DataTable dt = new DataTable();
+           // SqlDataAdapter.Fill(dt);
+
+
+            sqlConn.Close();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //on form load, grab database values and add id's to the dropdown box
+            string conString = "server=server1.logicalview.co.uk;user=c444WS259094;database=c444WS259094Colab;password=Friday@63;CharSet=utf8;";
+
+            MySqlConnection sqlConn = new MySqlConnection(conString);
+
+            string sqlStatement = "SELECT * FROM `tbl_Test`";
+
+            MySqlDataAdapter SqlDataAdapter = new MySqlDataAdapter(sqlStatement, sqlConn);
+
+            DataTable dtUsers = new DataTable();
+
+            sqlConn.Open();
+
+            SqlDataAdapter.Fill(dtUsers);
+
+            sqlConn.Close();
+
+            foreach (DataRow dr in dtUsers.Rows)
+            {
+                cboSelect.Items.Add(dr[0].ToString());
+      
+            }
+        }
+    }
+}
